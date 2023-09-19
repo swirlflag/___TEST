38b99ec60f52d36e64c5d275bdb4e8501a20faba
마승현 <swirlflag@nexon.co.kr>
2023-09-19 19:34:53 +0900

    save

--- 2023/0919formhooks/my-app/src/app/hooks.js
+++ 2023/0919formhooks/my-app/src/app/hooks.js
@@ -1,16 +1,115 @@
-import { useState, useMemo, useCallback } from "react";
-export const useRegistForm = () => {
-    const [phone, setPhone] = useState("1");
-    const [email, setEmail] = useState("");
+import { useMemo, useCallback, useReducer, useRef } from "react";
 
-    const form = useMemo(() => {
-        return {
-            phone,
-            email,
+export const useRegistForm = (initialState = {}) => {
+    initialState = {
+        phone: "",
+        phoneCode: false,
+        email: "",
+        myNumber: 1,
+        ...initialState,
+    };
+
+    const domDirectBindMap = useMemo(() => ({
+        phone: (ref) => {
+            const value = ref.target.value;
+            return value;
+        },
+        email: (ref) => {
+            const value = ref.target.value;
+            return value;
+        },
+    }));
+
+    const validater = useRef({
+        phone: (string) => {
+            const regexp = /^[0-9]{11}$/;
+            const value = regexp.test(string);
+            return value;
+        },
+        email: (string) => {
+            const regexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{1,}$/;
+            const value = regexp.test(string);
+            return value;
+        },
+    });
+
+    const setValidater = useCallback((type, fn) => {
+        validater.current = {
+            ...validater.current,
+            [type]: fn,
         };
-    }, [phone, email]);
+    });
 
-    const setForm = useCallback(() => {}, [phone, email]);
+    const pattern = useRef({
+        phone: (string) => {
+            const isRightLength = string.length < 12;
+            const isAllNumber = string === "" || /^\d+$/.test(string);
+            const isPattern = isRightLength && isAllNumber;
+            return isPattern;
+        },
+        email: () => {
+            return true;
+        },
+    });
 
-    return [form, setForm, setPhone];
+    const setPattern = useCallback((type, fn) => {
+        pattern.current = {
+            ...pattern.current,
+            [type]: fn,
+        };
+    });
+
+    const reducer = (state, action) => {
+        return {
+            ...state,
+            [action.type]: action.value,
+        };
+    };
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const valid = useMemo(
+        () =>
+            Object.entries(validater.current).reduce((p, [k, v]) => {
+                console.log(v);
+                p[k] = v ? v(state[k]) : true;
+                console.log(p[k]);
+                return p;
+            }, {}),
+        [state]
+    );
+
+    const set = useCallback(
+        (type, value) => {
+            const patternType = pattern.current[type];
+            const isPatternOk = patternType ? patternType?.(value) : true;
+            if (!isPatternOk) {
+                return;
+            }
+            dispatch({ type, value });
+        },
+        [pattern]
+    );
+
+    const bind = useCallback((type) => (ref) => {
+        const typeFn = domDirectBindMap?.[type];
+        const value = typeFn?.(ref);
+        typeFn && set(type, value);
+    });
+
+    const call = useCallback(async (key) => {
+        return fetch(key);
+    });
+
+    return {
+        state,
+        valid,
+        set,
+        bind,
+        dispatch,
+        validater,
+        pattern,
+        setValidater,
+        setPattern,
+    };
 };
--- 2023/0919formhooks/my-app/src/app/regist/page.jsx
+++ 2023/0919formhooks/my-app/src/app/regist/page.jsx
@@ -2,29 +2,48 @@
 import { useRegistForm } from "@/app/hooks.js";
 
 const Regist = () => {
-    const [form, setForm, setPhone] = useRegistForm();
+    const regForm = useRegistForm();
 
-    const sett = (target) => {
-        const value = target.target.value;
-        setPhone(value);
+    const { phone, email } = regForm.state;
+    const { bind, valid, validater } = regForm;
+
+    const onInputPhone = (ref) => {
+        const value = ref.target.value;
+        regForm.set("phone", value);
     };
-    console.log(form);
+
+    const onInputEmail = (ref) => {
+        const value = ref.target.value;
+        regForm.set("email", value);
+    };
 
     return (
         <>
-            <div>{form.phone}</div>
             <div>
-                <input type="text" value={form.phone} onChange={sett} />
+                phone: {phone}
+                <br />
+                email: {email}
+                <br />
+                <br />
+                valid phone: {valid.phone ? "true" : "false"}
+                <br />
+                valid email: {valid.email ? "true" : "false"}
             </div>
             <div>
-                <button
-                    onClick={() => {
-                        setPhone(form.phone + 1);
-                    }}
-                >
-                    hey
+                <input type="text" value={phone} onChange={onInputPhone} />
+            </div>
+            <div>
+                <input type="text" value={email} onChange={onInputEmail} />
+            </div>
+            <div>
+                <input type="text" value={phone} onChange={bind("phone")} />
+            </div>
+            <div>
+                <button onClick={() => regForm.set("phone", Math.random())}>
+                    1asaaaaa
                 </button>
             </div>
+            <button>최종 사전에약</button>
         </>
     );
 };
